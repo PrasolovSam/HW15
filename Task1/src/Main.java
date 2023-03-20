d9186f6f872794a8de5edac0e6f4739dd5bafbbe
Roman Prasolov <rvprasolov@me.com>
2023-03-20 14:17:16 +0400

    Strings

--- Task1/src/Main.java
+++ Task1/src/Main.java
@@ -1,9 +1,11 @@
 public class Main {
-    //todo Напишите код который распечатает букву и ее код для всех букв русского алфавита формат вывода:
-    // А-123
-    // Б-321...
-    // Коды даны для примера и не соответствуют действительности. Учитывайте, что нужно распечатать коды букв в двух регистрах!
     public static void main(String[] args) {
-        //todo код писать тут
+        String alphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+        for (int i = 0; i < alphabet.length(); i++) {
+            char letter = alphabet.charAt(i);
+            int code = (int) letter;
+            System.out.println(letter + " : " + code);
+        }
+
     }
 }
--- Task2/src/Main.java
+++ Task2/src/Main.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        //todo код писать тут. По желанию(рекомендуется) вынести код в методы.
+
+        printInfo();
+
+
+    }
+
+    public static String fullName(){
+        System.out.println("Введите ваше полное имя");
+        return new Scanner(System.in).nextLine().trim();
+    }
+
+    public static String firstName(){
+        String firstName = fullName().substring(0, ' ');
+        return firstName;
+    }
+    public static String lastName() {
+        String lastName = fullName().substring(fullName().lastIndexOf(' '));
+        return lastName;
+    }
+    public static String fatherName() {
+        String fatherName = fullName().substring(fullName().indexOf(' '), fullName().lastIndexOf(' '));
+        return fatherName;
+    }
+    public static void printInfo(){
+        System.out.println("Фамилия: " + lastName);
+        System.out.println("Имя: " + firstName);
+        System.out.println("Отчество: " + fatherName);
     }
 }
